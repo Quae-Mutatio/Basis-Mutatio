20b908898b82fa17c4f13c3d1fd1e5816f4ba709
Luke Gilfoyle <boo122333@gmail.com>
2020-11-05 16:02:50 +0000

    Initial Mod Setup
    Signed-off-by: Luke Gilfoyle <boo122333@gmail.com>

---
+++ src/main/java/dev/quae/mods/basis/BasisMutatio.java
@@ -0,0 +1,9 @@
+package dev.quae.mods.basis;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BasisMutatio.ID)
+public final class BasisMutatio {
+
+  public static final String ID = "basismutatio";
+}
